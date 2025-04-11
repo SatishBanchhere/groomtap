6d56bb3127723b0f4af77840eff7c5fe1db90778
Satish Banchhere <69459934+SatishBanchhere@users.noreply.github.com>
2025-04-11 23:33:47 +0530

    admin

---
+++ app/admin/page.js
@@ -0,0 +1,12 @@
+const ToolWrapper = () => (
+    <div style={{ height: "100vh" }}>
+      <iframe
+        src="https://doczadmin12.vercel.app/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Tool"
+      />
+    </div>
+  );
+  
+  export default ToolWrapper;
+  
