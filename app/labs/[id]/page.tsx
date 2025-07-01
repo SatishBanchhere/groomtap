29e42a315e19487f48e8fc2a1eaa531ab309fdd3
VpnUserAheMi06 <vpnuserahemi06@gmail.com>
2025-07-01 23:08:35 +0530

    some changes

--- app/labs/[id]/page.tsx
+++ app/labs/[id]/page.tsx
@@ -129,7 +129,6 @@ export default function LabDetailPage() {
 
     return (
         <>
-            {/* Structured Data for SEO */}
             <script
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{
